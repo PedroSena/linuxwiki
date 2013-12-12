9c93e76b167e042b318f1d9c58ef47d887a1e93f
Pedro Sena <sena.pedro@gmail.com>
2013-12-12 12:44:37 -0300

    FontAwesome integrated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Linuxwiki::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.assets.paths << "#{Rails.root}/app/assets/ico"
 end
