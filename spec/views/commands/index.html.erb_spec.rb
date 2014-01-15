77678b44496090c994f0d1143ed4b199e0bff74f
Pedro Sena <sena.pedro@gmail.com>
2014-01-15 10:28:40 -0300

    Fixed view test for index.hmtl.erb

--- spec/views/commands/index.html.erb_spec.rb
+++ spec/views/commands/index.html.erb_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe 'commands/index.html.erb' do
 
+  before(:each) do
+    assign(:searches, 5.times.map { FactoryGirl.create :search })
+  end
+
   def assert_section_and_children(section_name, section_selector, children_selector = '.fa-ul li')
+    render
     expect(rendered).to include section_name
     expect(rendered).to have_selector "#{section_selector}"
     expect(rendered).to have_selector "#{section_selector} #{children_selector}", count: 5
   end
 
   it 'displays the last 5 searches' do
-    render
     assert_section_and_children 'Latest Searches', '#section-latest-searches'
   end
 
   it 'displays the last 5 examples registered' do
-    render
     assert_section_and_children 'Latest Examples', '#section-latest-examples'
   end
 
